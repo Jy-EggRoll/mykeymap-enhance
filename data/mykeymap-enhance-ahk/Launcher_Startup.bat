63ff6735b303a88ab638ac4a414112819fda2f43
EggRoll <JyEggRoll@outlook.com>
2026-04-05 21:58:56 +0800

    💥 boom: 彻底移除启动器功能，存在无法修复的 bug（受限于 Windows 特性）

--- data/mykeymap-enhance-ahk/Launcher_Startup.bat
+++
@@ -1,5 +0,0 @@
-cd /d "%~dp0"
-
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "EggRollMyKeymapEnhanceLauncher" /t REG_SZ /d "\"%~dp0..\..\AutoHotkey64.exe\" \"%~dp0Launcher.ahk\"" /f
-
-start /b "" "%~dp0..\..\AutoHotkey64.exe" "%~dp0Launcher.ahk"
